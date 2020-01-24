f58daf97c1fb3e359ae8909aacc40edff7aead3f
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-01-24 21:52:04 +0100

    more code

--- src/main/scala/org/codecraftlabs/brl/Main.scala
+++ src/main/scala/org/codecraftlabs/brl/Main.scala
@@ -1,5 +1,11 @@
 package org.codecraftlabs.brl
 
+import org.apache.log4j.Logger
+
 object Main {
-  def main(args: Array[String]): Unit = ???
+
+  def main(args: Array[String]): Unit = {
+    @transient lazy val logger = Logger.getLogger(getClass.getName)
+    logger.info("Starting")
+  }
 }
