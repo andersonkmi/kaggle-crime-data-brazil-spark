f7a28896011c138c5fc72de0694236e8372c1c95
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-02-06 21:51:39 +0100

    more code

--- src/main/scala/org/codecraftlabs/ssp/Main.scala
+++ src/main/scala/org/codecraftlabs/ssp/Main.scala
@@ -7,7 +7,7 @@ import org.codecraftlabs.spark.utils.Timer.timed
 import org.codecraftlabs.ssp.data.SSPDataHandler.readContents
 
 object Main {
-  private val CsvFolder: String = "--csv-folder"
+  private val InputFolder: String = "--input-folder"
 
   def main(args: Array[String]): Unit = {
 
@@ -15,12 +15,12 @@ object Main {
     logger.info("Starting application")
 
     val argsMap = parseArgs(args)
-    val csvFolder = argsMap(CsvFolder)
+    val inputFolder = argsMap(InputFolder)
 
     val sparkSession: SparkSession = SparkSession.builder.appName("kaggle-crime-data-brazil-spark").master("local[*]").getOrCreate()
     import sparkSession.implicits._
 
     logger.info("Loading all CSV files")
-    val policeReports = timed("Reading all police reports", readContents("csv", s"$csvFolder/*.csv", sparkSession))
+    val policeReports = timed("Reading all police reports", readContents("csv", s"$inputFolder/*.csv", sparkSession))
   }
 }
