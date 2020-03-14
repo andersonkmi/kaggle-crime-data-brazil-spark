4e07e1ce22c0034abb4ab71eac4a42fbbb5a6e1d
Anderson Keiji Matsuki Ito <andersonkmi@acm.org>
2020-03-14 16:36:38 +0100

    more code

--- src/main/scala/org/codecraftlabs/ssp/Main.scala
+++ src/main/scala/org/codecraftlabs/ssp/Main.scala
@@ -11,7 +11,7 @@ object Main {
   private val GeneralInputFolder: String = "--general-input-folder"
   private val RegularReportFolder: String = "--regular-report-folder"
   private val DigitalReportFolder: String = "--digital-report-folder"
-  private val FileExtension: String = "*.csv"
+  private val FileExtension: String = "--file-extension"
   private val RowNumber: Int = 10
 
   def main(args: Array[String]): Unit = {
@@ -23,20 +23,21 @@ object Main {
     val generalInputFolder = argsMap(GeneralInputFolder)
     val regularReportFolder = argsMap(RegularReportFolder)
     val digitalReportFolder = argsMap(DigitalReportFolder)
+    val fileExtension = argsMap(FileExtension)
 
     val sparkSession: SparkSession = SparkSession.builder.appName("kaggle-crime-data-brazil-spark").master("local[*]").getOrCreate()
     import sparkSession.implicits._
 
     logger.info("Loading field description CSV")
-    val digitalReportFields = readContents(s"$generalInputFolder/$FileExtension", CSV, sparkSession, getDigitalReportDescriptionSchema)
+    val digitalReportFields = readContents(s"$generalInputFolder/$fileExtension", CSV, sparkSession, getDigitalReportDescriptionSchema)
     digitalReportFields.show(RowNumber)
 
     logger.info("Loading BO (standard reports) CSV files")
-    val policeReports = timed("Reading all police reports", readContents(s"$regularReportFolder/$FileExtension", CSV, sparkSession, getStandardPoliceReportSchema))
+    val policeReports = timed("Reading all police reports", readContents(s"$regularReportFolder/$fileExtension", CSV, sparkSession, getStandardPoliceReportSchema))
     policeReports.show(RowNumber)
 
     logger.info("Loading RDO (digital reports) CSV files")
-    val digitalPoliceReports = timed("Reading all digital police reports", readContents(s"$digitalReportFolder/$FileExtension", CSV, sparkSession, getDigitalPoliceReportSchema))
+    val digitalPoliceReports = timed("Reading all digital police reports", readContents(s"$digitalReportFolder/$fileExtension", CSV, sparkSession, getDigitalPoliceReportSchema))
     digitalPoliceReports.show(RowNumber)
   }
 }
